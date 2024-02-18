f69dad09976d5da930baf972f077481c381fcdda
Fujikouki <c1202599@st.kanazawa-it.ac.jp>
2024-02-18 20:16:26 +0900

    データベースバージョン１

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ FB = firebase()
 
 
 
+
 @app.route('/',methods=['GET'])
 def welcome():
     if 'usr' in session:
@@ -36,7 +37,9 @@ def sinup():
         _password = request.form['password']
         isFlag, usr = FB.Signup(name="藤季", email=_username, password=_password)
         if isFlag:
-            session["usr"] = _username
+            session["usr"] = usr.uid
+            print(session["usr"])
+            create_user(usr.uid, "藤季", _username)
             return redirect(url_for('Introduction'))
         else:
             return render_template('welcome.html', sinup_failed=not isFlag, sinup_error=usr)
@@ -93,60 +96,66 @@ def save_preference():
 
     return jsonify({'status': 'success'})
 
-#データベース
+
+
+# ユーザーテーブル
 class User(db.Model):
     __tablename__ = 'user'
-    uid = db.Column(db.Integer, primary_key=True,)
+    uid = db.Column(db.String(50), primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
-    favoriteStyle = db.relationship('closet',secondary='style', back_populates='style')
-    closet_items = db.relationship('Closet', backref='owner', lazy=True)
-    followers = db.relationship('Follower', foreign_keys='Follower.follower_uid', backref='follower', lazy=True)
-    followed = db.relationship('Follower', foreign_keys='Follower.followed_uid', backref='followed', lazy=True)
-    def __str__(self):
-        return f'uif:{self.uid},User:{self.name},Email:{self.email},Closet:{self.closet_items},Followers:{self.followers},Followed:{self.followed}'
-    
-class Style(db.Model):
-    __tablename__ = 'style'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_uid = db.Column(db.Integer, db.ForeignKey('user.uid'))
-    closet_id = db.Column(db.Integer, db.ForeignKey('closet.id'))
-    style = db.Column(db.String(50), nullable=False)
+    closet_items = db.relationship('Closet', backref='owner', lazy='dynamic')
+    favorite_styles = db.relationship('Style', secondary='user_style_link', back_populates='users')
 
-
+# クローゼットテーブル
 class Closet(db.Model):
     __tablename__ = 'closet'
-    #必須のカラム
     id = db.Column(db.Integer, primary_key=True)
-    user_uid = db.Column(db.Integer, db.ForeignKey('user.uid'), nullable=False)
+    uid = db.Column(db.String(50), db.ForeignKey('user.uid'), nullable=False)
     category = db.Column(db.String(50), nullable=False)
     brand = db.Column(db.String(50))
     image = db.Column(db.String(100), nullable=False)
-    favoriteStyle = db.relationship('user',secondary='style', back_populates='favoriteStyle')
-
+    clothes_styles = db.relationship('Style', secondary='closet_style_link', back_populates='closets')
     #オプションのカラム
     size = db.Column(db.String(50))
     price = db.Column(db.Float)
     purchase_date = db.Column(db.DateTime)
     note = db.Column(db.Text)
 
-    def __str__(self):
-        return f'id:{self.id},User:{self.user_uid},Category:{self.category},Color:{self.color},Brand:{self.brand},Season:{self.season},Size:{self.size},Price:{self.price},Purchase_date:{self.purchase_date},Image:{self.image},Note:{self.note}'
-
+# フォロワーテーブル
 class Follower(db.Model):
     __tablename__ = 'follower'
     id = db.Column(db.Integer, primary_key=True)
-    follower_uid = db.Column(db.Integer, db.ForeignKey('user.uid'), nullable=False)
-    followed_uid = db.Column(db.Integer, db.ForeignKey('user.uid'), nullable=False)
-    def __str__(self):
-        return f'id:{self.id},Follower:{self.follower_uid},Followed:{self.followed_uid}'
+    follower_uid = db.Column(db.String(50), db.ForeignKey('user.uid'), nullable=False)
+    followed_uid = db.Column(db.String(50), db.ForeignKey('user.uid'), nullable=False)
+
+# スタイルテーブル
+class Style(db.Model):
+    __tablename__ = 'style'
+    id = db.Column(db.Integer, primary_key=True)
+    style_name = db.Column(db.String(50), nullable=False)
+    users = db.relationship('User', secondary='user_style_link', back_populates='favorite_styles')
+    closets = db.relationship('Closet', secondary='closet_style_link', back_populates='clothes_styles')
+
+# ユーザーとスタイルの中間テーブル
+user_style_link = db.Table('user_style_link',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.uid', name='fk_user_style_user_id'), primary_key=True),
+    db.Column('style_id', db.Integer, db.ForeignKey('style.id', name='fk_user_style_style_id'), primary_key=True)
+)
+
+# クローゼットとスタイルの中間テーブル
+closet_style_link = db.Table('closet_style_link',
+    db.Column('closet_id', db.Integer, db.ForeignKey('closet.id', name='fk_closet_style_closet_id'), primary_key=True),
+    db.Column('style_id', db.Integer, db.ForeignKey('style.id', name='fk_closet_style_style_id'), primary_key=True)
+)
+
     
 def create_user(uid,name, email):
     with app.app_context():
         user = User(uid=uid, name=name, email=email)
         db.session.add(user)
         db.session.commit()
-        return user
+        print("成功")
 
 def create_closet(user_uid, category, brand, image, style, size=None, price=None, purchase_date=None, note=None):
     with app.app_context():
