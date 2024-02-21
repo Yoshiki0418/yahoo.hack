a3e1bc5f390977aec7af2abdde616a4b6d674885
hiromu <hiromureve1522@icloud.com>
2024-02-22 02:13:31 +0900

    データをnextButtonを押すことでsaveAreaにある情報がすべて保存されるようにしました。

--- static/js/Introduction2.js
+++ static/js/Introduction2.js
@@ -14,8 +14,34 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     nextButton.addEventListener('click', function() {
-        window.location.href = '/home';
+        let formData = new FormData();
+    
+        imagesData.forEach((data, index) => {
+            formData.append(`images[${index}]`, data.file); // 画像ファイルを追加
+            formData.append(`infos[${index}]`, data.info); // 関連情報を追加
+        });
+    
+        for (let [key, value] of formData.entries()) {
+            console.log(`${key}: ${value}`);
+        }
+    
+        fetch('/save-all-images', {
+            method: 'POST',
+            body: formData,
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+            window.location.href = '/home';
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
     });
+    
+    
+
+    
 
     // ファイルアップロードハンドラー
     function handleFileUpload() {
@@ -31,8 +57,6 @@ document.addEventListener('DOMContentLoaded', function() {
             changePhotoButton.style.backgroundColor = 'rgba(0, 0, 255, 0.5)';
             changePhotoButton.style.color = 'white'; // テキストの色を白に変更
 
-            // ここにプレビュー機能やその他の処理を追加する
-            previewImage(); // プレビュー関数を呼び出す
         }
     }
 
@@ -40,6 +64,12 @@ document.addEventListener('DOMContentLoaded', function() {
         if (isImageUploaded) {
             let formData = new FormData();
             formData.append('image', imageUpload.files[0]);
+    
+            // カーソルをローディングアイコンに変更し、changePhotoボタンを無効化
+            document.body.style.cursor = 'wait';
+            backgroundTransparencyButton.style.cursor = 'wait';
+            changePhotoButton.disabled = true; // changePhotoボタンを無効化
+            changePhotoButton.style.cursor = 'default'; // マウスカーソルはデフォルトのまま
     
             fetch('/remove-background', {
                 method: 'POST',
@@ -54,13 +84,30 @@ document.addEventListener('DOMContentLoaded', function() {
             .then(data => {
                 processedImageUrl = data.file_path; // 背景削除後の画像URLをグローバル変数に保存
                 previewProcessedImg(); // 背景削除後の画像プレビューを更新する関数を呼び出し
+    
+                // ボタンを非アクティブ状態に戻す
                 backgroundTransparencyButton.disabled = true;
                 backgroundTransparencyButton.style.backgroundColor = ''; 
                 backgroundTransparencyButton.style.color = '';
+    
+                // カーソルを元に戻し、changePhotoボタンを再度有効化
+                document.body.style.cursor = 'default';
+                backgroundTransparencyButton.style.cursor = 'default';
+                changePhotoButton.disabled = false; // changePhotoボタンを再度有効化
+                changePhotoButton.style.cursor = 'pointer'; // マウスカーソルをポインターに戻す
             })
-            .catch(error => console.error('Error:', error));
+            .catch(error => {
+                console.error('Error:', error);
+                // エラー発生時もカーソルを元に戻し、ボタンを再度有効化
+                document.body.style.cursor = 'default';
+                backgroundTransparencyButton.style.cursor = 'default';
+                changePhotoButton.disabled = false; // changePhotoボタンを再度有効化
+                changePhotoButton.style.cursor = 'pointer'; // マウスカーソルをポインターに戻す
+            });
         }
     });
+    
+    
     
 
     function previewImage() {
@@ -75,6 +122,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 if (!img) { // imgタグがまだなければ作成
                     const newImg = document.createElement('img');
                     document.getElementById('imagePreview').appendChild(newImg);
+                    img = newImg; // この行を追加
                 }
                 img.src = processedImageUrl; // 背景削除後の画像URLを設定
                 document.getElementById('imageUploadLabel').style.display = 'none';
@@ -115,34 +163,34 @@ document.addEventListener('DOMContentLoaded', function() {
         document.getElementById('imageUpload').click();
     });
 
+    // 画像ファイルとその情報を保持する配列
+    let imagesData = [];
+
     saveButton.addEventListener('click', function() {
         const saveArea = document.getElementById('saveArea');
         const file = imageUpload.files[0];
         if (file) {
             const reader = new FileReader();
-            let formData = new FormData();
-            formData.append('image', file);
-            reader.onload = function() { // ここを変更しました
+            reader.onload = function() {
                 const saveArea = document.getElementById('saveArea');
                 const imageContainer = document.createElement('div');
                 imageContainer.className = 'image-container';
                 saveArea.appendChild(imageContainer);
     
                 const img = document.createElement('img');
-                img.src = processedImageUrl; // 背景削除後の画像URLを使用
+                img.src = processedImageUrl; // FileReaderから読み込んだ画像データを使用
                 img.className = 'saveArea-image';
                 imageContainer.appendChild(img);
     
+                // ボタンの状態更新
                 backgroundTransparencyButton.disabled = true;
                 backgroundTransparencyButton.style.backgroundColor = ''; 
-                backgroundTransparencyButton.style.color = '';
+                backgroundTransparencyButton.style.color = ''; 
+                changePhotoButton.disabled = true;
+                changePhotoButton.style.backgroundColor = ''; 
+                changePhotoButton.style.color = ''; 
     
-                 // ボタンをアクティブにする
-                 changePhotoButton.disabled = true;
-                 changePhotoButton.style.backgroundColor = ''; 
-                 changePhotoButton.style.color = ''; 
-    
-                //Json形式でデータを保存
+                // Json形式でデータを保存
                 const infoObject = Array.from(inputFields).reduce((acc, field, index) => {
                     const fieldName = document.querySelector(`.information-${index + 1}`).textContent.trim();
                     const fieldValue = field.value.trim() === '' ? 'none' : field.value.trim();
@@ -152,24 +200,16 @@ document.addEventListener('DOMContentLoaded', function() {
     
                 const infoJson = JSON.stringify(infoObject); // オブジェクトをJSON文字列に変換
                 img.dataset.info = infoJson;
-                formData.append('info', infoJson);
+
+                imagesData.push({ file: file, info: infoJson }); // 画像と情報を配列に追加
+
+                 // 追加された要素のインデックス（配列の最後の要素なので、length - 1となる）
+                const index = imagesData.length - 1;
+
+                // HTML要素にインデックスを紐付ける
+                imageContainer.setAttribute('data-index', index);
     
-                // サーバーにデータを送信
-                fetch('/save-image', {
-                    method: 'POST',
-                    body: formData, // FormDataオブジェクトをそのまま使用
-                }).then(response => {
-                    if (response.ok) {
-                        return response.json(); // 正常なレスポンスをJSONとしてパース
-                    }
-                    throw new Error('Network response was not ok.'); // レスポンスが異常な場合はエラーを投げる
-                }).then(data => {
-                    console.log('Success:', data);
-                    // 成功した場合の処理
-                }).catch(error => {
-                    console.error('Error:', error);
-                });
-    
+                // 画像クリックでモーダル表示
                 img.addEventListener('click', function() {
                     const modal = document.getElementById('imageInfoModal');
                     const modalText = document.getElementById('modal-info-text');
@@ -177,15 +217,30 @@ document.addEventListener('DOMContentLoaded', function() {
                     modal.style.display = 'flex'; // モーダルを表示
                 });
     
+                // 削除ボタンの追加
                 const deleteButton = document.createElement('button');
                 deleteButton.className = 'delete-button';
                 deleteButton.textContent = '×';
                 imageContainer.appendChild(deleteButton);
     
                 deleteButton.addEventListener('click', function() {
+                    // 削除する要素のインデックスを取得
+                    const index = parseInt(imageContainer.getAttribute('data-index'), 10); // 10進数として解析
+                
+                    // インデックスを使用して配列から要素を削除
+                    imagesData.splice(index, 1); // indexの位置から1要素を削除
+                
+                    // 画像コンテナをDOMから削除
                     saveArea.removeChild(imageContainer);
+                
+                    // imagesDataのインデックスを更新する（必要な場合）
+                    document.querySelectorAll('.image-container').forEach((container, newIndex) => {
+                        container.setAttribute('data-index', newIndex); // 新しいインデックスを設定
+                    });
                 });
+                
     
+                // プレビューとフォームのリセット
                 document.getElementById('imagePreview').style.backgroundImage = '';
                 document.getElementById('imageUploadLabel').style.display = 'block';
                 imageUpload.value = '';
@@ -197,6 +252,7 @@ document.addEventListener('DOMContentLoaded', function() {
             reader.readAsDataURL(file);
         }
     });
+    
     
 
     function resetInputFields() {
